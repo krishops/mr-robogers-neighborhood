6b7edd7a167741d7f889c20e138143dd2b1580de
Kris Hopper <hopperdavis@gmail.com>
2021-06-11 10:57:41 -0700

    passes fourth test

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 function mrRoboger(number) {
-  // let array = [number]
+  let array = [];
   // array.forEach(function(1) {
   //   array.push(number+1)
   //   console.log(array)
   // })
 //I need a for loop!!
-for (i = 0; i<= number; i+=1 )
-console.log(i)
+for (i = 0; i<= number; i+=1 ) {
+  array.push(i)
+console.log(i, array)
+
 
 
 
 
   // array.push(number);
   // console.log(array)
+}
 }
 
 //I need the inputted number to be the index position and the last number 
