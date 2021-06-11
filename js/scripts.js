1d7a17f699020b8a0a4465b2aff194cfcb7ed921
Kris Hopper <hopperdavis@gmail.com>
2021-06-11 11:44:27 -0700

    WIP: trouble-shooting utility logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,65 @@
-function mrRoboger(number) {
+
+function checkNumber(element) {
+  if (element === 1 || element === 2 || element === 3) {
+    return true;
+  } 
+    else {
+      return false;
+    }
+  }
   let array = [];
+//creates an array
+function formArray(number) {
+for (i = 0; i <= number; i +=1 ) {
+  array.push(i)
+console.log(i, array)
+}
+}
+
+
+
+
+
+
+
+// newArray.forEach(toString()) 
+//   // array.forEach(function(element) {
+//     // if (array.includes([1])) {
+//     //   return true
+
+//     }
+//   }
+
+  
+// console.log()
+
+
+
+
+//recognizes 1, 2, and 3
+// function checkNumber(element) {
+//   if (element === 1 || element === 2 || element === 3) {
+//     return true;
+//   }
+//   else {
+//     return false
+//   }
+// }
+
+//   let marker = 0
+//   for (let i = 0; i < array.length; i++)
+
+// }
+
+  // array.push(number);
+  // console.log(array)
+
+
+//I need the inputted number to be the i position and the last number 
+
   // array.forEach(function(1) {
   //   array.push(number+1)
   //   console.log(array)
   // })
-//I need a for loop!!
-for (i = 0; i<= number; i+=1 ) {
-  array.push(i)
-console.log(i, array)
-
-
-
-
-
-  // array.push(number);
-  // console.log(array)
-}
-}
-
-//I need the inputted number to be the index position and the last number 
+// I need a for loop
+//for (let i = 0; i <= array.length; i +=1)
